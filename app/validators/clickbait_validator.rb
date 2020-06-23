b6814045cac9ef56ce90f0d4d2fe0dc06b82d381
Rene Avila <reneavila1993@gmail.com>
2020-06-23 00:24:09 +0000

    Automatically backed up by Learn

--- app/validators/clickbait_validator.rb
+++ app/validators/clickbait_validator.rb
@@ -1,10 +1,8 @@
 class ClickbaitValidator < ActiveModel::Validator 
   def validate record
     requirements = ["Won't Believe", "Secret", "Top[number]", "Guess"]
-    if record.title
-      if requirements.find {|element| record.title.include?(element)} == nil
+    if requirements.find {|element| record.title.include?(element)} == nil
         record.errors[:title] << "No clickbait"
-      end
     end
   end
 end
